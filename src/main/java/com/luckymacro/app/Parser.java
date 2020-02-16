39852b3b6aefb5943caffffb415d5c9d8148c22f
Deon Tan <deonjtan@gmail.com>
2020-02-16 00:11:53 +0000

    wip stuck on eq

--- src/main/java/com/luckymacro/app/Parser.java
+++ src/main/java/com/luckymacro/app/Parser.java
@@ -17,6 +17,8 @@ public class Parser {
     private static final String incSp = "@SP\nM=M+1\n";
     private static final String decSp = "@SP\nM=M-1\n";
     private static final String derefSp = "@SP\nA=M\n";
+    private static final String readSpToD = "A=M\nD=M\n";
+    private static final String deleteStackEntry = "M=0\n";
 
     private static String readVariableToD(String memSegment, String segAddr) {
         return String.format("@%1$s\nD=A\n", segAddr);
@@ -31,8 +33,6 @@ public class Parser {
     }
 
     private static String add() {
-        String readSpToD = "A=M\nD=M\n";
-        String deleteStackEntry = "M=0\n";
         String readSpToMAndAddToD = "A=M\nM=M+D\n";
         return decSp +
             readSpToD +
@@ -42,14 +42,69 @@ public class Parser {
             incSp;
     }
 
+    private static String eq() {
+        String readSpToMAndSubFromD = "A=M\nM=M-D\n";
+        String ifEqThen1Else0 = "D=M\n@1\nD;JEQ\n@0";
+        return decSp +
+            readSpToD +
+            deleteStackEntry +
+            decSp +
+            readSpToMAndSubFromD +
+            ifEqThen1Else0 +
+            incSp;
+    }
+
+    private static String lt() {
+        return "";
+    }
+
+    private static String gt() {
+        return "";
+    }
+
+    private static String sub() {
+        String readSpToMAndSubFromD = "A=M\nM=M-D\n";
+        return decSp +
+            readSpToD +
+            deleteStackEntry +
+            decSp +
+            readSpToMAndAddToD +
+            incSp;
+    }
+
+    private static String neg() {
+        return "";
+    }
+
+    private static String and() {
+        return "";
+    }
+
+    private static String or() {
+        return "";
+    }
+
+    private static String not() {
+        return "";
+    }
+
     private static String dispatch(String cmd) {
         String[] words = cmd.split("\\s");
         String command = words[0];
-        if (command.contentEquals("push")) {
-            return push(cmd);
-        } else {
-            return add();
+        String result = "";
+        switch (command) {
+            case "push": result = push(cmd); break;
+            case "eq": result = eq(); break;
+            case "lt": result = lt(); break;
+            case "gt": result = gt(); break;
+            case "add": result = add(); break;
+            case "sub": result = sub(); break;
+            case "neg": result = neg(); break;
+            case "and": result = and(); break;
+            case "or": result = or(); break;
+            case "not": result = not(); break;
         }
+        return result;
     }
 
     public static boolean lineToKeep(String line) {
