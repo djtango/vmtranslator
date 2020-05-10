04ee0ccb4cfb7ec841a4734a6c683781ac862d96
Deon Tan <deonjtan@gmail.com>
2020-05-10 16:38:35 +0100

    test sub

---
+++ resources/test/Sub.asm
@@ -0,0 +1,112 @@
+// push constant 2
+@2
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+M=0
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+// push constant 20
+@20
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 10
+@10
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+M=0
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+// push constant 3
+@3
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 3
+@3
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+M=0
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+// push constant 3
+@3
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 4
+@4
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+A=M
+D=M
+M=0
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
